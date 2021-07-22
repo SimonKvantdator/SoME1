9e2d3e5d7ebe6c6059debe06bf68add521ad7e3b
Simon Jacobsson <simjac@student.chalmers.com>
2021-07-22 22:50:04 +0200

    blergh

--- generate_index.sh
+++ generate_index.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Generate .md file from .tex
-pandoc --standalone --from=latex main.tex --output=index.md --to=gfm --mathjax
+pandoc --standalone --from=latex main.tex --output=index.md --to=gfm
 
 # Add some preamble at the beginning of the file
 echo '---
-usemathjax: true
+usemathjax:true
 ---
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>

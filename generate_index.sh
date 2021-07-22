f507e07b11cab549efdd3d3dc0b0d0631ce5e67a
Simon Jacobsson <simjac@student.chalmers.com>
2021-07-22 22:57:30 +0200

    blergh

--- generate_index.sh
+++ generate_index.sh
@@ -7,8 +7,16 @@ pandoc --standalone --from=latex main.tex --output=index.md --to=gfm
 echo '---
 usemathjax:true
 ---
-<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+<script type="text/javascript">
+window.MathJax = {
+  tex: {
+    packages: ['base', 'ams']
+  },
+  loader: {
+    load: ['ui/menu', '[tex]/ams']
+  }
+};
+</script>
 
 [comment]: <> (This .md file is automatically generated from main.tex)
 
--- index.md
+++ index.md
@@ -1,8 +1,16 @@
 ---
 usemathjax:true
 ---
-<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+<script type="text/javascript">
+window.MathJax = {
+  tex: {
+    packages: [base, ams]
+  },
+  loader: {
+    load: [ui/menu, [tex]/ams]
+  }
+};
+</script>
 
 [comment]: <> (This .md file is automatically generated from main.tex)
 

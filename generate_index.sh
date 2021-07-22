8e7130e4a626a4ab803cfa2bfc2a69a29d228b1e
Simon Jacobsson <simjac@student.chalmers.com>
2021-07-22 19:49:50 +0200

    blergh

--- generate_index.sh
+++ generate_index.sh
@@ -6,13 +6,13 @@ pandoc -s main.tex -o index.md --mathjax
 # Add some preamble at the beginning of the file
 echo '---
 usemathjax: true
-header-includes:
-  - \usepackage{tensor}
 ---
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 
 [comment]: <> (This .md file is automatically generated from main.tex)
+
+\(\require{tensor}\)
 
 ' | cat - index.md > temp && mv temp index.md
 
--- index.md
+++ index.md
@@ -1,12 +1,12 @@
 ---
 usemathjax: true
-header-includes:
-  - \usepackage{tensor}
 ---
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 
 [comment]: <> (This .md file is automatically generated from main.tex)
+
+\(\require{tensor}\)
 
 
 If you ask a physics professor what a tensor is, they will likely tell

cab81b91a20a5a5e796f86492a68b87e25bc0655
Simon Jacobsson <simjac@student.chalmers.com>
2021-07-22 19:35:39 +0200

    added packages in pandoc conversion

--- generate_index.sh
+++ generate_index.sh
@@ -7,6 +7,10 @@ pandoc -s main.tex -o index.md --mathjax
 echo '---
 usemathjax: true
 ---
+header-includes:
+  - `\usepackage{physics,tensor}`{=latex}
+---
+---
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 

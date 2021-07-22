118fcd51c4b988d1e8103aee55dfbda8b740b04c
Simon Jacobsson <simjac@student.chalmers.com>
2021-07-22 23:05:45 +0200

    blergh

--- generate_index.sh
+++ generate_index.sh
@@ -7,7 +7,9 @@ pandoc --standalone --from=latex main.tex --output=index.md --to=gfm
 echo '---
 usemathjax:true
 ---
-<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+<script type="text/javascript" id="MathJax-script" async
+  src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js">
+</script>
 
 [comment]: <> (This .md file is automatically generated from main.tex)
 
--- index.md
+++ index.md
@@ -1,7 +1,9 @@
 ---
 usemathjax:true
 ---
-<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+<script type="text/javascript" id="MathJax-script" async
+  src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js">
+</script>
 
 [comment]: <> (This .md file is automatically generated from main.tex)
 

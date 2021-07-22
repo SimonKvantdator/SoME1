ee280b54268d063f5deca8e8377146556b5c0eaf
Simon Jacobsson <simjac@student.chalmers.com>
2021-07-22 19:47:36 +0200

    blergh

--- generate_index.sh
+++ generate_index.sh
@@ -6,10 +6,8 @@ pandoc -s main.tex -o index.md --mathjax
 # Add some preamble at the beginning of the file
 echo '---
 usemathjax: true
----
 header-includes:
-  - \usepackage{physics,tensor}
----
+  - \usepackage{tensor}
 ---
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
--- index.md
+++ index.md
@@ -1,9 +1,7 @@
 ---
 usemathjax: true
----
 header-includes:
-  - \usepackage{physics,tensor}
----
+  - \usepackage{tensor}
 ---
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>

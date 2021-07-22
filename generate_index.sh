334bfb9c9f5b6c746ee4f660e8abd1583de43525
Simon Jacobsson <simjac@student.chalmers.com>
2021-07-22 19:27:33 +0200

    blergh

--- generate_index.sh
+++ generate_index.sh
@@ -4,12 +4,14 @@
 pandoc -s main.tex -o index.md --mathjax
 
 # Add some preamble at the beginning of the file
-echo "[comment]: <> (This .md file is automatically generated from main.tex)
----
+echo "---
 usemathjax: true
 ---
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+
+[comment]: <> (This .md file is automatically generated from main.tex)
+
 " | cat - index.md > temp && mv temp index.md
 
 
--- index.md
+++ index.md
@@ -1,9 +1,11 @@
-[comment]: <> (This .md file is automatically generated from main.tex)
 ---
 usemathjax: true
 ---
 <script src=https://polyfill.io/v3/polyfill.min.js?features=es6></script>
 <script id=MathJax-script async src=https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js></script>
+
+[comment]: <> (This .md file is automatically generated from main.tex)
+
 
 If you ask a physics professor what a tensor is, they will likely tell
 you that "it is something that transforms like a tensor." I was

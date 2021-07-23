8c055517e1dc71fcd85a30c6a7c9bd726feda731
Simon Jacobsson <simjac@student.chalmers.com>
2021-07-23 09:01:38 +0200

    blergh

--- generate_index.sh
+++ generate_index.sh
@@ -8,8 +8,7 @@ pandoc -s main.tex --o index.md
 echo '---
 usemathjax:true
 ---
-<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=default"></script>
 
 [comment]: <> (This .md file is automatically generated from main.tex)
 
